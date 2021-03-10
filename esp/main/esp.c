3069c66174ddacc92120e91880aa31940109dbfb
hermlon <dev.hermlon@yamx.net>
2021-03-10 11:58:10 +0100

    connect to wifi

--- esp/main/esp.c
+++ esp/main/esp.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include "wifi_station.h"
+#include "lwip/err.h"
+#include "nvs_flash.h"
 
-void app_main(void)
-{
+/* The examples use WiFi configuration that you can set via project configuration menu
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
 
+void nvs_init() {
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ESP_ERROR_CHECK(nvs_flash_init());
+  }
+}
+
+void app_main(void) {
+  nvs_init();
+  if(wifi_init_sta(EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_MAXIMUM_RETRY) == ESP_OK) {
+    
+  }
+  else {
+    // start AP and allow for wifi scan
+  }
 }
---
+++ esp/main/wifi_station.h
@@ -0,0 +1,8 @@
+#ifndef WIFI_STATION_H
+#define  WIFI_STATION_H
+
+#include <stdint.h>
+#include "lwip/err.h"
+
+esp_err_t wifi_init_sta(char wifi_ssid[32], char wifi_pass[64], int retries);
+#endif
