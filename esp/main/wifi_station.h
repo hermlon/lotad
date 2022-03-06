1e522bfa4d25883cde1c1e787313994efaca17bd
hermlon <dev.hermlon@yamx.net>
2022-03-06 16:43:53 +0100

    removed old wifi station

--- esp/main/wifi_station.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOTAD_WIFI_STATION_H
-#define  LOTAD_WIFI_STATION_H
-
-#include <stdint.h>
-#include "lwip/err.h"
-
-esp_err_t wifi_init_sta(char wifi_ssid[32], char wifi_pass[64], int retries);
-#endif
